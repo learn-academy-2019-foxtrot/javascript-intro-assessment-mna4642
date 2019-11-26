80993cc83bbc84c884b336bf274f24a8755fa406
Ubuntu <ubuntu@ip-172-31-42-17.us-east-2.compute.internal>
2019-11-26 22:14:55 +0000

    submitting assessment

--- js-intro.js
+++ js-intro.js
@@ -7,37 +7,48 @@ var mantra = "Be the dev"
 
 // 1a. Write the code that determines if there is a 'B' in mantra.
 
-
+console.log(mantra.includes("B"))
 
 // 1b. Write the code that determines if there is an 'x' in mantra.
 
-
+console.log(mantra.includes("x"))
 
 // 1c. Write the code that determines if there is a 'v' in mantra.
 
-
+console.log(mantra.includes("v"))
 
 // 1d. STRETCH: Write a function that can take two arguments - a string and a letter and returns a sentence telling the user if the letter exists within the string.
+ 
+const myName = (letter) => {
+    if (letter.includes('Michael')){
+        return letter + " exists within the string.";
+    } else{
+        return letter + " does not exist within the string.";
+    }
+};
 
-
+console.log(myName('M'));
 
 // ------------------------------ Consider the following variables:
 
 var myDog = "Snoopy"
 var myCat = "Garfield"
 
+
 // 2a. Create a function that returns the longer of the two variables. Expected output: "Garfield"
+console.log(myCat)
 
 
 
 
 // 2b. Create a function that takes in two arguments and returns an array containing the two variables. Expected output: ["Snoopy", "Garfield"]
 
+console.log(myDog.concat(myCat));
 
 
 
 // 2c. STRETCH: Write a function that accepts two variables and returns a string in all lowercase letters. Expected output: "snoopy garfield"
-
+console.log((myDog + myCat).toLowerCase());
 
 
 
@@ -50,7 +61,8 @@ var myMessage = "Learning is fun!"
 
 
 // 3b. Write the code that logs each letter of the message using map.
-
+var mapMessage = myMessage.map((value => value));
+console.log(mapMessage);
 
 
 // 3c. STRETCH: Write the code that logs each letter of the message using a while loop.
@@ -67,8 +79,19 @@ var myMessage = "Learning is fun!"
 var testString = "thisisateststring"
 
 // 4a. Write a function that takes the variable as an argument and returns the string without vowels. Expected output: "thsstststrng"
-
-
+const vowelRemover = (str) => {
+    let vowels = ["a","e","i","o","u"]
+    let newString = ""
+    for (i = 0; i <str.length; i++){
+        if (vowels.includes(str[i]) !== true){
+            newString += str[i];
+        }else {
+            continue;
+        }
+    }
+    return newString;
+}
+console.log(vowelRemover(testString));
 
 
 
@@ -84,12 +107,14 @@ var testString = "thisisateststring"
 var toonimals = [ {name: "Itchy", animal: "mouse"}, {name: "Stimpy", animal: "cat"}, {name: "Daffy", animal: "duck"}, {name: "Scratchy", animal: "cat"}, {name: "Ren", animal: "dog"}, {name: "Felix", animal: "cat"}]
 
 // 5a. Write a function that takes in the toonimals array as an argument and returns an array with only the toon objects that are cats. Expected output: [ { name: "Stimpy", animal: "cat" }, { name: "Scratchy", animal: "cat" }, { name: "Felix", animal: "cat" } ]
-
+var  onlyCats = toonimals.filter((value) => value.animal === "cat")
+console.log(onlyCats)
 
 
 
 //5b. Write a function that returns only the names of all the non-cats. Expected output: "Itchy" "Daffy" "Ren"
-
+var nonCats = toonimals.map((value) => value.name)
+console.log(nonCats)
 
 
 
--- js-intro.md
+++ js-intro.md
@@ -6,22 +6,33 @@ Answer the following questions. First, without external resources. Challenge you
 1. What is a function? Why would you use one?
 
   Your answer:
+  A function is an object that is responsible to perform a task. 
 
   Researched answer:
+  A function is a set of instructions detailing how to do a task. It is important
+
+
+
+
 
 
 
 2. List 5 examples of JavaScript array methods.
 
   Your answer:
-
+  var cars = ["Honda","Ford","Toyota"]
+  var numbers = [1,2,3,4,5]
+  
   Researched answer:
+  const
 
 
 
 3. Explain the difference between console.log() and return.
 
   Your answer:
+  Console.log() runs and print out the result of the statement made by a function, class, array, variable, etc. 
+  Return is a statement that return the result of a function, class, array, variable, etc. 
 
   Researched answer:
 
@@ -30,6 +41,7 @@ Answer the following questions. First, without external resources. Challenge you
 4. In regards to functions, what is an argument? What is the difference between an argument and a variable?
 
   Your answer:
+  Argument is a object that represents a function to 
 
   Researched answer:
 
@@ -45,9 +57,11 @@ Answer the following questions. First, without external resources. Challenge you
 
 6. Give a brief description of JavaScript as if you were explaining it to someone who doesn't know anything about programming.
 
-  Your answer:
+  Your answer: 
+  Javascript is programming language that helps web developers create a dynamic implmentation using HTML and CSS.
 
 
 7. What is one thing we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: 
+  
